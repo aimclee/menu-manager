160c14deece5d2ac76803b1ff8fc2baea991d698
aimclee <dhy04057@likelion.org>
2022-04-28 19:51:16 +0900

    TODO menu added

--- src/js/index.js
+++ src/js/index.js
@@ -2,27 +2,74 @@
 
 
 // TODO 메뉴 추가
-// - [ ] 메뉴의 이름을 입력받고 엔터키 입력으로 추가한다.
-// - [ ] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
-// - [ ] 총 메뉴 갯수를 count하여 상단에 보여준다.
-// - [ ] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
-// - [ ] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+// - [x] 메뉴의 이름을 입력받고 엔터키 입력으로 추가한다.
+// - [x] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
+// - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
+// - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
+// - [x] 사용자 입력값이 빈 값이라면 추가되지 않는다.
+
+// $ : js에서 dom element를 가져올 때 관용적으로 사용
+const $ = (selector) => document.querySelector(selector);
 
 function App() {
   // form 태그가 자동으로 전송되는걸 막아준다.
-  document
-    .querySelector("#espresso-menu-form")
+  $("#espresso-menu-form")
     .addEventListener("submit", (e) => {
       e.preventDefault();
     });
 
+
+
   // 메뉴의 이름을 입력받기
+
+
+
+
+
   document
     .querySelector("#espresso-menu-name")
     .addEventListener("keypress", (e) => {
-      if (e.key === "Enter") {
-        console.log(document.querySelector("#espresso-menu-name").value);
+      if (e.key !== "Enter") {
+        return;
+      }
+
+      if ($("#espresso-menu-name").value === "") {
+        alert("값을 입력해주세요");
+        return;
+      }
+
+
+      const espressoMenuName = $("#espresso-menu-name").value;
+      const menuItemTemplate = () => {
+        return `<li class="menu-list-item d-flex items-center py-2">
+        <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+        >
+          수정
+        </button>
+        <button
+          type="button"
+          class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+        >
+          삭제
+        </button>
+      </li>`;
       };
+
+      $("#espresso-menu-list").insertAdjacentHTML(
+        "afterbegin",
+        menuItemTemplate(espressoMenuName)
+      );
+
+      const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+      $(".menu-count").innerText = `총 ${menuCount}개`
+
+      $('#espresso-menu-name').value = "";
+
+
+
     })
 }
 
