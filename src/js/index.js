b72ff6c7bae8fe627aeaf633ffacf60d6813f445
aimclee <dhy04057@likelion.org>
2022-05-04 22:30:15 +0900

    menu update 기능 added

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@
 
 // TODO 메뉴 추가
 // - [x] 메뉴의 이름을 입력받고 엔터키 입력으로 추가한다.
-// - [ ] 메뉴의 이름을 입력받고 확인 버튼을 클릭하면 메뉴를 추가한다.
+// - [x] 메뉴의 이름을 입력받고 확인 버튼을 클릭하면 메뉴를 추가한다.
 // - [x] 추가되는 메뉴의 아래 마크업은 `<ul id="espresso-menu-list" class="mt-3 pl-0"></ul>` 안에 삽입해야 한다.
 // - [x] 총 메뉴 갯수를 count하여 상단에 보여준다.
 // - [x] 메뉴가 추가되고 나면, input은 빈 값으로 초기화한다.
@@ -13,12 +13,25 @@
 const $ = (selector) => document.querySelector(selector);
 
 function App() {
+
+  // TODO 메뉴 수정
+  // - [x] 메뉴의 수정 버튼 클릭 이벤트를 받고, 메뉴 수정하는 모달창(prompt)이 뜬다.
+  // - [x] 모달창에서 신규메뉴명을 입력 받고, 확인버튼을 누르면 메뉴가 수정된다.
+
+  // event delegation(위임)
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      const $menuName = e.target.closest("li").querySelector(".menu-name")
+      const updatedMenuName = prompt("메뉴명을 수정하세요", $menuName.innerText);
+      $menuName.innerText = updatedMenuName;
+    }
+  });
+
   // form 태그가 자동으로 전송되는걸 막아준다.
   $("#espresso-menu-form")
     .addEventListener("submit", (e) => {
       e.preventDefault();
     });
-
 
   const addMenuName = () => {
     if ($("#espresso-menu-name").value === "") {
@@ -66,14 +79,11 @@ function App() {
 
 
   // 메뉴의 이름을 입력받기
-  document
-    .querySelector("#espresso-menu-name")
+  $("#espresso-menu-name")
     .addEventListener("keypress", (e) => {
       if (e.key !== "Enter") {
         return;
       }
-
-
       addMenuName();
     });
 }
